707bc8271d4dc03ded38136307134fcba71ad7dc
soumojitbagchi001 <soumojitbagchi001@gmail.com>
2025-11-20 17:00:43 +0530

    update display random greetings by click button js

--- javascript/dom/day3/script.js
+++ javascript/dom/day3/script.js
@@ -8,18 +8,25 @@ let main = document.querySelector('body')
 let btn = document.querySelector('button')
 btn.style.position='absolute'
 btn.style.zIndex = '10'
+let arr=['hello','hola','bonjour','नमस्ते','hallo','হ্যালো','konichiwa','ni hao']
 btn.addEventListener('click',function(){
   let r =Math.floor(Math.random()*100)
   let t =Math.floor(Math.random()*100)
   let d= Math.floor(Math.random()*360)
-  let div = document.createElement('div')
-  div.style.height='40px'
-  div.style.width='40px'
-  div.style.backgroundColor=`rgb(${Math.floor(Math.random()*100)},${Math.floor(Math.random()*100)},${Math.floor(Math.random()*100)})`
-  div.style.position='absolute'
-  div.style.top=t+'%'
-  div.style.right=r+'%'
-  div.style.rotate=d+'deg'
-  main.appendChild(div)
+  let h1 = document.createElement('h1')
+  h1.style.height='60px'
+  h1.style.width='fit-content'
+  h1.style.backgroundColor=`rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`
+  h1.style.position='absolute'
+  h1.style.top=t+'%'
+  h1.style.right=r+'%'
+  h1.style.rotate=d+'deg'
+  main.appendChild(h1)
+  h1.innerHTML=arr[Math.floor(Math.random()*arr.length)]
+  h1.style.alignItems='center'
+  h1.style.justifyContent='center'
+  h1.style.padding='10px 20px'
+  h1.style.borderRadius='5px'
+  console.log(h1)
   console.log(r,d,t)
 })
