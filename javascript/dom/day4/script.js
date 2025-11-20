20fe22cb97378db1141344f55e797755d5eff907
soumojitbagchi001 <soumojitbagchi001@gmail.com>
2025-11-21 01:55:22 +0530

    download button

--- javascript/dom/day4/script.js
+++ javascript/dom/day4/script.js
@@ -1,30 +1,4 @@
-// console.log('hello')
-// setTimeout(function(){
-//   console.log('ki re bokachoda')
-// },3000)
-// setTimeout(function(){
-//   console.log('bero be bara')
-// },5000)
-// setTimeout(function(){
-//   console.log('banchod ekhono achis tui')
-// },8000)
-// let btn = document.querySelector('button')
-// let h1 =  document.querySelector('h1')
-// btn.innerHTML='full name'
-// btn.addEventListener('click',function(){
-//   h1.innerHTML='revealing'
-//   setTimeout(function(){
-//     h1.innerHTML='soumojit bagchi'
-//   },3000)
-// })
-// let a =1
-// var int = setInterval(function(){
-//   console.log(a)
-//   a++
-// },10)
-// setTimeout(function(){
-//   clearInterval(int)
-// },1000)
+
 let btn = document.querySelector('button')
 let m=50 +Math.floor(Math.random()*50)
 let num=0
